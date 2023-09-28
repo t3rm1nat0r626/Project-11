d1e08550cfcfa9d9f22b5ef6e2df927485138390
t3rm1nat0r626 <133919053+t3rm1nat0r626@users.noreply.github.com>
2023-09-28 17:28:00 -0400

    Add files via upload

---
+++ project-11/scripts/java.js
@@ -0,0 +1,187 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Get all the necessary DOM elements
+const form = document.getElementById('exampleForm')
+const submitButton = document.querySelector('.submit')
+const successMessage = document.getElementById('form-submitted-msg')
+
+// Store all form elements in an array by spreading the elements property of the form
+const formElements = [ ...form.elements ]
+
+// Create function to check if all form elements are valid
+const allInputsValid = () => {
+  const valid = formElements.every((element) => {
+    if (element.nodeName === 'SELECT') {
+      return element.value !== 'Please select an option'
+    } else {
+      return element.checkValidity()
+    }
+  })
+
+
+  return valid
+}
+
+/*
+  Create function to handle the change event on a form element,
+  enabling the submit button if all form elements are valid
+*/
+const handleChange = () => {
+  if (allInputsValid()) {
+    submitButton.removeAttribute('disabled', '')
+  } else {
+    submitButton.setAttribute('disabled', '')
+  }
+}
+
+// Create function to handle the submit event on the form
+const handleSubmit = (e) => {
+
+  e.preventDefault()
+
+  formElements.forEach((element) => {
+
+    if (!element.checkValidity()
+          && element.nodeName !== 'BUTTON'
+          && element.nodeName !== 'SELECT'  
+          && element.type !== 'checkbox'
+          && element.type !== 'radio'
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+
+      element.nextElementSibling.style.display = 'block'
+
+      element.previousElementSibling.style.color = 'red'
+    }
+
+
+    // Reset to original colors if valid
+    if (element.checkValidity()
+          && element.nodeName !== 'BUTTON'
+          && element.nodeName !== 'SELECT'
+          && element.type !== 'checkbox'
+          && element.type !== 'radio'
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#CED4DA'
+
+      element.nextElementSibling.style.display = 'none'
+
+      element.previousElementSibling.style.color = '#212529'
+    }
+
+
+    if (!element.checkValidity()
+          && (element.type === 'checkbox'
+              || element.type === 'radio')
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+    }
+
+
+    // Reset to original colors if valid
+    if (element.checkValidity()
+          && (element.type === 'checkbox'
+              || element.type === 'radio')
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#212529'
+    }
+
+
+    if (element.nodeName === 'SELECT'
+          && element.value === 'Please select an option'
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+
+      element.nextElementSibling.style.display = 'block'
+
+      element.previousElementSibling.style.color = 'red'
+    }
+
+
+    // Reset to original colors if valid
+    if (element.nodeName === 'SELECT'
+          && element.value !== 'Please select an option'
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#CED4DA'
+
+
+      element.nextElementSibling.style.display = 'none'
+
+
+      element.previousElementSibling.style.color = '#212529'
+    }
+  })
+
+
+  if (allInputsValid()) {
+    successMessage.style.display = 'block'
+
+    form.reset()
+
+    submitButton.setAttribute('disabled', '')
+
+    setTimeout(() => {
+      successMessage.style.display = 'none'
+    }, 3000)
+  }
+}
+
+// Add event listener to each form element
+formElements.forEach((element) => {
+  element.addEventListener('change', handleChange)
+})
+
+// Add submit listener to the form
+form.addEventListener('submit', (e) => handleSubmit(e))
+
+import firebase from "firebase/app";
+import "firebase/compat/storage";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  storageBucket: '[gs://project-11-4a147.appspot.com]'
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = firebase.storage();
+
